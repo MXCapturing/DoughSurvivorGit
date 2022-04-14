85da5641e65b04ce44acbc9b5566b574ae303048
Unknown <MXCapturing@gmail.com>
2022-04-15 01:19:04 +1200

    Added shooting and aim varience
    + Basic bullet
    + Added aiming offset
    + Started Entity base class

--- Assets/Scripts/Bullets/BasicBullet.cs
+++ Assets/Scripts/Bullets/BasicBullet.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BasicBullet : MonoBehaviour
 {
+    public float travelDistance;
+    public int damage;
+    public float travelTime;
+    float travelSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        travelSpeed = travelDistance / travelTime;
+        Invoke("Destroy", travelTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * travelSpeed * Time.deltaTime);
+    }
+
+    void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            
+        }
     }
 }
--- Assets/Scripts/Player/BulletShoot.cs
+++ Assets/Scripts/Player/BulletShoot.cs
@@ -6,6 +6,13 @@ public class BulletShoot : MonoBehaviour
 {
     public float fireRate;
     float fireTimer;
+    public GameObject bullet;
+
+    float offset;
+    public float offsetMax;
+    public float offsetIncreaseRate;
+    public float offsetDecreaseRate;
+    public float straightShotTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +25,48 @@ public class BulletShoot : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            OffsetUp();
             if(fireTimer <= 0)
             {
                 Debug.Log("LeftClick");
+                float offsetRand = Random.Range(-offset, offset);
+                Quaternion offsetShot = Quaternion.Euler(0, 0, offsetRand);
+                Instantiate(bullet, transform.position, transform.rotation * offsetShot);
                 fireTimer = fireRate;
             }
+        }
+        else
+        {
+            OffsetDown();
         }
 
         if(fireTimer > 0)
         {
             fireTimer -= Time.deltaTime;
+        }
+    }
+
+    void OffsetUp()
+    {
+        if (offset < offsetMax)
+        {
+            offset += offsetIncreaseRate * Time.deltaTime;
+        }
+        else if (offset > offsetMax)
+        {
+            offset = offsetMax;
+        }
+    }
+
+    void OffsetDown()
+    {
+        if (offset > 0)
+        {
+            offset -= offsetDecreaseRate * Time.deltaTime;
+        }
+        else if (offset < 0)
+        {
+            offset = 0;
         }
     }
 }
